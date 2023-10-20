517adbbc9c3d234a056954a30a14ec28a0032c2e
Yu Qin <sirius.qinyu@gmail.com>
2023-10-20 11:06:57 +0900

    Abstract away section grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,9 @@
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { Database } from "@/database/database.types";
+import SectionGrid from "@/components/SectionGrid";
 
 export default async function Home() {
-    const supabase = createClientComponentClient<Database>();
-
-    const { data: testSections } = await supabase
-        .from("test_section")
-        .select("*");
-
     return (
         <main className="">
-            <div>
-                {testSections?.map((el) => (
-                    <div className="m-2" key={el.id}>
-                        {el.section_name}
-                    </div>
-                ))}
-            </div>
+            <SectionGrid />
         </main>
     );
 }
