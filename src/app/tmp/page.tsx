057037640121c0d5af302574fbb4dcd8afa3983e
Yu Qin <sirius.qinyu@gmail.com>
2023-10-25 11:08:37 +0900

    Change story and add button component

--- src/app/tmp/page.tsx
+++ src/app/tmp/page.tsx
@@ -1,12 +1,40 @@
-import AudioPlayer from '@/components/AudioPlayer'
+import AudioPlayer, { Transcription } from '@/components/AudioPlayer'
 
-const sentences = ['sentence 1', 'sentence 2', 'sentence 3', 'sentence 4']
-const breakpoints = [2, 4, 6]
+const transcription = [
+    {
+        type: 'filler',
+        endTime: 2,
+    },
+    {
+        type: 'content',
+        content: '这是第1句话',
+        endTime: 4,
+    },
+    {
+        type: 'content',
+        content: '这是第2句话',
+        endTime: 6,
+    },
+    {
+        type: 'content',
+        content: '这是第3句话',
+        endTime: 8,
+    },
+    {
+        type: 'content',
+        content: '这是第4句话',
+        endTime: 9999,
+    },
+] as Transcription[]
+
 export default async function Page() {
     return (
-        <main className="m-4">
-            <div>This is tmp page</div>
-            <AudioPlayer sentences={sentences} breakpoints={breakpoints} />
+        <main className="m-4 flex flex-col items-center">
+            <div className="m-4">This is tmp page</div>
+            <AudioPlayer
+                audioUrl={'https://assets.riyu.love/audios/2020_12_n3_1_1.mp3'}
+                transcription={transcription}
+            />
         </main>
     )
 }
--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,30 +1,60 @@
 'use client'
 
 import { useState } from 'react'
+import { Button } from './ui/button'
+import clsx from 'clsx'
+
+type Filler = {
+    type: 'filler'
+    endTime: number
+}
+
+type Content = {
+    type: 'content'
+    content: string
+    endTime: number
+}
+
+export type Transcription = Filler | Content
 
 type Props = {
-    sentences: string[]
-    breakpoints: number[]
+    audioUrl: string
+    transcription: Transcription[]
 }
-export default function AudioPlayer({ sentences, breakpoints }: Props) {
+export default function AudioPlayer({ audioUrl, transcription }: Props) {
     const [audio, setAudio] = useState<HTMLAudioElement | null>(null)
-    const [sentenceIndex, setSentenceIndex] = useState(0)
+    const [transcriptionPartIndex, setTranscriptionPart] = useState(0)
     const [understood, setUnderstood] = useState(false)
     const [showSentence, setShowSentence] = useState(false)
     const [repeatTime, setRepeatTime] = useState(0)
+    const [contentIndex, setContentIndex] = useState(0)
+
+    const transcriptionPart = transcription[transcriptionPartIndex]
+    const breakpoints = transcription.map((el) => el.endTime)
+
+    const contentLength = transcription.filter(
+        (el) => el.type === 'content',
+    ).length
 
     const handleAudioTimeUpdate = () => {
         const currentTime = audio?.currentTime
         if (currentTime === undefined) {
             return
         }
-        const currentBreakpoint = breakpoints[sentenceIndex] ?? Infinity
-        const lastBreakpoint = breakpoints[sentenceIndex - 1] ?? 0
+        const currentBreakpoint =
+            breakpoints[transcriptionPartIndex] ?? Infinity
+        const lastBreakpoint = breakpoints[transcriptionPartIndex - 1] ?? 0
 
         if (currentTime < currentBreakpoint) return
 
+        if (transcriptionPart.type === 'filler') {
+            setTranscriptionPart(transcriptionPartIndex + 1)
+            return
+        }
+
         if (understood) {
-            setSentenceIndex(sentenceIndex + 1)
+            setTranscriptionPart(transcriptionPartIndex + 1)
+            setContentIndex(contentIndex + 1)
             setUnderstood(false)
             setShowSentence(false)
             setRepeatTime(0)
@@ -36,30 +66,57 @@ export default function AudioPlayer({ sentences, breakpoints }: Props) {
 
     const handleAudioEnded = () => {
         if (!understood) {
-            const lastBreakpoint = breakpoints[sentenceIndex - 1] ?? 0
+            const lastBreakpoint = breakpoints[transcriptionPartIndex - 1] ?? 0
             audio!.currentTime = lastBreakpoint
             audio?.play()
         }
     }
 
     return (
-        <div className="m-4">
+        <div className="bg-red-50 p-4 flex flex-col rounded">
+            <div className="flex justify-between">
+                <div className="">
+                    句子：{contentIndex + 1}/{contentLength}
+                </div>
+                <div
+                    className={clsx({
+                        'text-gray-500': transcriptionPart.type === 'filler',
+                    })}
+                >
+                    重复次数: {repeatTime}
+                </div>
+            </div>
+
             <audio
+                className="my-4"
                 controls
                 ref={setAudio}
-                src="https://assets.riyu.love/audios/2020_12_n3_1_1.mp3"
+                src={audioUrl}
                 onTimeUpdate={handleAudioTimeUpdate}
                 onEnded={handleAudioEnded}
             />
-            <div>current sentence: {sentences[sentenceIndex]}</div>
 
-            <div>repeat time: {repeatTime}</div>
+            <div className="my-5 mb-7 flex justify-center items-center gap-10">
+                <Button
+                    onClick={() => setShowSentence(true)}
+                    disabled={
+                        showSentence || transcriptionPart.type === 'filler'
+                    }
+                >
+                    原文
+                </Button>
 
-            <button onClick={() => setUnderstood(true)}>听懂了</button>
+                <Button
+                    onClick={() => setUnderstood(true)}
+                    disabled={understood || transcriptionPart.type === 'filler'}
+                >
+                    听懂了
+                </Button>
+            </div>
 
-            <button onClick={() => setShowSentence(true)}>show sentence</button>
-
-            {showSentence && <div>{sentences[sentenceIndex]}</div>}
+            {showSentence && transcriptionPart.type !== 'filler' && (
+                <div>{transcriptionPart.content}</div>
+            )}
         </div>
     )
 }
