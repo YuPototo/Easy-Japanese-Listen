aa8f153cb5702e6314de15394a744053f43d3eb4
Yu Qin <sirius.qinyu@gmail.com>
2023-11-09 11:02:35 +0900

    Use dark orange theme

---
+++ src/app/playStyle/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+
+export default async function Page() {
+    return (
+        <main className="">
+            <h1>Use this page to play style</h1>
+
+            <Button variant="destructive">hello </Button>
+
+            <Button>hello </Button>
+        </main>
+    )
+}
