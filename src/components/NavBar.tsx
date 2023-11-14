eb29a937591814880914d1a1c245ea7381a2e377
Yu Qin <sirius.qinyu@gmail.com>
2023-11-15 08:35:05 +0900

    Add navbar bg

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function NavBar() {
     return (
-        <div className="fixed top-0 inset-x-0 h-fit border-b z-50 py-2">
+        <div className="fixed bg-slate-700 top-0 inset-x-0 h-fit border-b z-50 py-2">
             <div className="container h-full mx-auto flex items-center justify-between gap-2">
                 <Link href="/" className="flex gap-2 items-center">
                     <Image
