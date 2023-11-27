2d503beec4ae12e4b2df82535bd7e0ecb7cdc4b2
Yu Qin <sirius.qinyu@gmail.com>
2023-11-27 16:04:03 +0900

    fix data id error

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,70 +1,97 @@
--- Insert album
-insert into
-    public .album (id, album_title)
-values
-    (100, 'Test Track Postion Album'),
-    (101, 'Test Error Album');
-
--- Insert track for album 100
-insert into
-    public .track (
-        id,
-        track_title,
-        transcription,
-        storage_path,
-        position_index,
-        album_id
-    )
-values
-    (
-        1001,
-        'Track 1',
-        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
-        'track_1.mp3',
-        1,
-        100
-    ),
-    (
-        1002,
-        'Track 2',
-        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
-        'track_2.mp3',
-        2,
-        100
-    ),
-    (
-        1003,
-        'Track 3',
-        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
-        'track_1.mp3',
-        3,
-        100
-    );
-
--- Insert track for album 101
-insert into
-    public .track (
-        id,
-        track_title,
-        transcription,
-        storage_path,
-        position_index,
-        album_id
-    )
-values
-    (
-        1011,
-        'Track 1: transcription validation error',
-        '[{"type999":"content","text":"这是第一句话","endTime":9999}]',
-        'track_1.mp3',
-        1,
-        101
-    ),
-    (
-        1012,
-        'Track 2: audio not found',
-        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
-        'not-found.mp3',
-        2,
-        101
-    );
+-- 所有 seed 的数据都已经导出，暂时不需要 seed 了。
+-- -- Insert album
+-- insert into
+--     public .album (album_title)
+-- values
+--     ('Test Track Postion Album'),
+--     ('Test Error Album');
+-- -- Insert track for album 100
+-- insert into
+--     public .track (
+--         track_title,
+--         transcription,
+--         storage_path,
+--         position_index,
+--         album_id
+--     )
+-- values
+--     (
+--         'Track 1',
+--         '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+--         'track_1.mp3',
+--         1,
+--         (
+--             select
+--                 id
+--             from
+--                 public .album
+--             where
+--                 album_title = 'Test Track Postion Album'
+--         )
+--     ),
+--     (
+--         'Track 2',
+--         '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+--         'track_2.mp3',
+--         2,
+--         (
+--             select
+--                 id
+--             from
+--                 public .album
+--             where
+--                 album_title = 'Test Track Postion Album'
+--         )
+--     ),
+--     (
+--         'Track 3',
+--         '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+--         'track_1.mp3',
+--         3,
+--         (
+--             select
+--                 id
+--             from
+--                 public .album
+--             where
+--                 album_title = 'Test Track Postion Album'
+--         )
+--     );
+-- -- Insert track for album 101
+-- insert into
+--     public .track (
+--         track_title,
+--         transcription,
+--         storage_path,
+--         position_index,
+--         album_id
+--     )
+-- values
+--     (
+--         'Track 1: transcription validation error',
+--         '[{"type999":"content","text":"这是第一句话","endTime":9999}]',
+--         'track_1.mp3',
+--         1,
+--         (
+--             select
+--                 id
+--             from
+--                 public .album
+--             where
+--                 album_title = 'Test Error Album'
+--         )
+--     ),
+--     (
+--         'Track 2: audio not found',
+--         '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+--         'not-found.mp3',
+--         2,
+--         (
+--             select
+--                 id
+--             from
+--                 public .album
+--             where
+--                 album_title = 'Test Error Album'
+--         )
+--     );
