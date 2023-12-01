f3dd2062e1a2ce04b10c3943fa7cbae00426b98c
Yu Qin <sirius.qinyu@gmail.com>
2023-12-01 17:42:05 +0900

    Add endtime increase check

--- src/components/edit/TranscriptionEditor.tsx
+++ src/components/edit/TranscriptionEditor.tsx
@@ -184,5 +184,22 @@ function validateDraft(draft: unknown) {
         }
     }
 
+    // validate that the latter sentence's end time should be larger than the previous one
+    for (let i = 1; i < draft.length; i++) {
+        const prevSentence = draft[i - 1] as TranscriptionPart
+        const currSentence = draft[i] as TranscriptionPart
+
+        if (prevSentence.endTime > currSentence.endTime) {
+            return {
+                success: false,
+                error: `Sentence ${i} end time ${
+                    prevSentence.endTime
+                } is smaller than sentence ${i - 1} end time ${
+                    currSentence.endTime
+                }`,
+            }
+        }
+    }
+
     return { success: true, error: null }
 }
