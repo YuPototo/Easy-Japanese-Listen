c132d6501eedb3c7feabca054025c25dd00fe7f9
Yu Qin <sirius.qinyu@gmail.com>
2023-12-10 21:11:13 +0900

    backup data

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ export const IMAGE_BUCKET_NAME = 'images'
 
 export const DEFAULT_ALUM_COVER = 'default_cover.png'
 
-export const SPEAKER_LIST = ['男', '女  ', 'A', 'B']
+export const SPEAKER_LIST = ['男', '女', 'A', 'B']
