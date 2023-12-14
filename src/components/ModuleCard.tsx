222e25f5ea0e458ab5e7aebffb06002b56a6036f
Yu Qin <sirius.qinyu@gmail.com>
2023-12-14 17:29:34 +0900

    don'e use type for Link in ModuleCard

--- src/components/ModuleCard.tsx
+++ src/components/ModuleCard.tsx
@@ -1,21 +1,20 @@
 import Link from 'next/link'
-import type { Route } from 'next'
 
-// I can't use a Type here because I need to pass generic to types
-// Or can I?
+type Props = {
+    title: string
+    href: string
+    children: React.ReactNode
+}
 
 export default function ModuleCard<T extends string>({
     title,
     href,
     children,
-}: {
-    title: string
-    href: Route<T> | URL
-    children: React.ReactNode
-}) {
+}: Props) {
     return (
         <Link
             className="flex h-36 w-36 flex-col items-center justify-start rounded-lg bg-zinc-100 px-10 shadow"
+            //@ts-expect-error
             href={href}
         >
             <div className="flex w-36 flex-grow items-center justify-center rounded-t-lg bg-green-400">
