42fa49a11bc2b2f73364ee040b7297b2f5b5f576
Yu Qin <sirius.qinyu@gmail.com>
2023-12-19 14:55:14 +0900

    New album page

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -4,7 +4,8 @@ insert into
 values
     ('Test Album', TRUE),
     ('Test Track Position Album', TRUE),
-    ('Test Error Album', TRUE);
+    ('Test Error Album', TRUE),
+    ('Many Tracks', TRUE);
 
 -- Insert track for album: Test Track Position Album
 insert into
@@ -119,3 +120,434 @@ values
                 title = 'Test Error Album'
         )
     );
+
+-- Insert track for album: Many Tracks
+insert into
+    public .track (
+    title,
+        transcription,
+        storage_path,
+        position_index,
+        album_id
+    ) 
+values (
+        'Track 1',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        1,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 2',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        2,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 3',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        3,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 4',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        4,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 5',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        5,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 6',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        6,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 7',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        7,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 8',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        8,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 9',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        9,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 10',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        10,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 11',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        11,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 12',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        12,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 13',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        13,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 14',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        14,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 15',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        15,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 16',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        16,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 17',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        17,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 18',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        18,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 19',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        19,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 20',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        20,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 21',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        21,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 22',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        22,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 23',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        23,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 24',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        24,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 25',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        25,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 26',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        26,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 27',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        27,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 28',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        28,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 29',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        29,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    ),
+    (
+        'Track 30',
+        '[{"type":"content","text":"这是第一句话","endTime":9999}]',
+        'track_1.mp3',
+        30,
+        (
+            select
+                id
+            from
+                public .album
+            where
+                title = 'Many Tracks'
+        )
+    )
+    ;
