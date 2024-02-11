5515ecebf5b6f48f22c157f3fb2074967ba512fe
Yu Qin <sirius.qinyu@gmail.com>
2024-02-11 11:33:06 +0900

    rename ui page

--- src/app/ui/page.tsx
+++ src/app/ui/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default async function Page() {
     return (
         <main className="">
-            <Link href="/styles/button">Button</Link>
+            <Link href="/ui/button">Button</Link>
         </main>
     )
 }
