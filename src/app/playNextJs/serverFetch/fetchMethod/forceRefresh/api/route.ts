95db5ff439a04d51116ec9fa3401ef4bae75c1c0
Yu Qin <sirius.qinyu@gmail.com>
2024-02-14 09:07:04 +0900

    remove play next.js page

--- src/app/playNextJs/serverFetch/fetchMethod/forceRefresh/api/route.ts
+++
@@ -1,6 +0,0 @@
-export async function GET() {
-    console.log('GET /playNextJs/serverFetch/fetchMethod/forceRefresh/api')
-    const data = { message: 'This is from a force refresh api' }
-
-    return Response.json({ data })
-}
--- src/app/playNextJs/serverFetch/fetchMethod/success/api/route.ts
+++
@@ -1,6 +0,0 @@
-export async function GET() {
-    console.log('GET /playNextJs/serverFetch/fetchMethod/success/api')
-    const data = { message: 'Cached Api' }
-
-    return Response.json({ data })
-}
